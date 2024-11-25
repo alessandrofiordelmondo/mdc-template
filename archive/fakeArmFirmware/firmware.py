c4d2a03bfe5859e9c72b8f606b74eb8282adec96
alessandrofiordelmondo <alessandrofiordelmondo92@gmail.com>
2024-11-25 10:09:15 +0100

    release 2.0 2024-01-01

--- archive/fakeArmFirmware/firmware.py
+++ archive/fakeArmFirmware/firmware.py
@@ -9,6 +9,11 @@ def checkTheBirds():
     # check the birds
     return 
 
+def doSomethingNew() -> None:
+    # Do something new
+    return 
+
 if __name__ == "__main__":
     if checkTheBirds():
         moveTheArms()
+        doSomethingNew()
